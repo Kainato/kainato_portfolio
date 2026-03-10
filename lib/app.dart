3a76fe2575fec7a69439db2814d9098ca7c5a767
Kainato <caiocaladaraujo@gmail.com>
2026-03-10 15:50:55 -0300

    adiciona a classe HomePage com uma tela inicial e estrutura básica

--- lib/app.dart
+++ lib/app.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
 
+import 'app/home/home_page.dart';
+
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(body: Center(child: Text('Hello World!'))),
+      home: HomePage(),
     );
   }
 }
