bf8d91eaf5ff8181b1dbacd158521f1ecf125c76
Kainato <caiocaladaraujo@gmail.com>
2026-08-21 08:22:55 -0300

    Adicionar documentação ao ponto de entrada da aplicação Flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:kainato_portfolio/app.dart';
 
+/// ### O ponto de entrada da aplicação Flutter.
+///
+/// A função *[main]* é responsável por iniciar a execução do aplicativo, chamando a função *[runApp]* e passando uma instância do widget *[MainApp]* como argumento. Isso configura o ambiente necessário para renderizar a interface do usuário e iniciar o ciclo de vida do aplicativo.
 void main() => runApp(const MainApp());
