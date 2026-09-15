ff04b66ab848e76c2b1cb0d3389c38f7848cbf39
Caio Calado <caiocaladaraujo@gmail.com>
2026-09-15 18:59:01 -0300

    Configura estratégia de URL 'usePathUrlStrategy'
    Configure o Flutter para web para utilizar URLs baseadas em caminho (path-based) em vez de URLs baseadas em hash, chamando `usePathUrlStrategy()` na função `main()`. Isso melhora a estética da URL e é mais vantajoso para SEO.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_web_plugins/url_strategy.dart';
 import 'package:kainato_portfolio/app.dart';
 
 /// ### O ponto de entrada da aplicação Flutter.
 ///
 /// A função *[main]* é responsável por iniciar a execução do aplicativo, chamando a função *[runApp]* e passando uma instância do widget *[MainApp]* como argumento. Isso configura o ambiente necessário para renderizar a interface do usuário e iniciar o ciclo de vida do aplicativo.
-void main() => runApp(const MainApp());
+void main() {
+  usePathUrlStrategy();
+  runApp(const MainApp());
+}
