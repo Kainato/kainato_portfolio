71c5aec1211b2d0bfcf81614f3d79c8444b755dd
Caio Calado <caiocaladaraujo@gmail.com>
2026-09-21 14:34:36 -0300

    Adiciona widget KpDrawerListTile
    Widget exclusivo para o Drawer

---
+++ lib/widgets/base/kp_drawer_list_tile.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class KpDrawerListTile extends StatelessWidget {
+  /// Indica se o item está selecionado.
+  final bool selected;
+  /// O texto exibido no item.
+  final String label;
+  /// O rótulo semântico (ajuda) do item.
+  final String semanticLabel;
+  /// O ícone exibido no item.
+  final IconData icon;
+  /// Callback acionado quando o item é tocado.
+  final VoidCallback onTap;
+
+  /// `KpDrawerListTile` é um widget exclusivo de item de drawer personalizado que exibe um ícone, um rótulo e suporta seleção e ação de toque.
+  ///
+  /// ### Parâmetros
+  /// - `selected`: Indica se o item está selecionado.
+  /// - `label`: O texto exibido no item.
+  /// - `semanticLabel`: O rótulo semântico do item.
+  /// - `icon`: O ícone exibido no item.
+  /// - `onTap`: Callback acionado quando o item é tocado.
+  const KpDrawerListTile({
+    super.key,
+    required this.selected,
+    required this.label,
+    required this.semanticLabel,
+    required this.icon,
+    required this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Semantics(
+      button: true,
+      selected: selected,
+      label: semanticLabel,
+      child: ListTile(
+        leading: Icon(icon),
+        title: Text(label),
+        selected: selected,
+        onTap: onTap,
+      ),
+    );
+  }
+}
